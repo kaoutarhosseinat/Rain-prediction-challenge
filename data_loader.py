0ad8f17982ba1c024be023a23a8f0da14dcbc2aa
Kaoutar Hosseinat <kaoutarhosseinat@gmail.com>
2024-09-27 21:24:53 +0100

    data loading done

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,15 @@
+#importing labraries
+import numpy as np 
 import pandas as pd
-data = pd.read_csv('c:/Users/kawthar/Downloads/train.csv')
+#importing dataset
+data = pd.read_csv(r'c:/Users/kawthar/Downloads/train.csv')
+print(data.columns)
 print(data.head())
 print(data.isnull().sum())
 print(data.describe)
+print(data[['Evaporation','Sunshine']])
+x=data.iloc[:,[2,3,4,5,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22]].values
+y=data.iloc[:,-1].values
+y=y.reshape(-1,1)
+print(x)
+print(y)
